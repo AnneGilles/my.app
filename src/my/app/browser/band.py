c14e14bac7cce66e1d178b01ee2dccb819d027e3
Christoph Scheid <c@shri.de>
2011-12-14 17:30:23 +0100

    metadata & cleanup

--- src/my/app/browser/band.py
+++ src/my/app/browser/band.py
@@ -1,10 +1,5 @@
 from plumber import plumber
-from yafowil.base import UNSET
 from cone.tile import tile
-#from my.app.browser.utils import (
-#    make_url,
-#    format_date,
-#    )
 from cone.app.browser.layout import ProtectedContentTile
 from cone.app.browser.form import (
     Form,
@@ -45,14 +40,6 @@ class BandTile(ProtectedContentTile):
             return [account]
         return account
 
-    # @property
-    # def registered(self):
-    #     return format_date(self.model.metadata.beginning)
-
-    # @property
-    # def unregistered(self):
-    #     return format_date(self.model.metadata.duedate)
-
 
 class BandForm(object):
     __metaclass__ = plumber
@@ -72,17 +59,12 @@ class BandAddForm(BandForm, Form):
     __plumbing__ = AddPart
 
     def save(self, widget, data):
-        #print "BandAddForm: trying to save form data"
-        print "setting self.model.__name__ = 'foo'"
-        self.model.__name__ = 'foo'
-        #print "setting self.model to be parent of self.model.name"
+        self.model.__name__ = self.model.uuid
         self.model.parent[str(self.model.uuid)] = self.model
-        #print "setting attr title: " + data.fetch('bandform.title').extracted
         self.model.attrs['title'] = data.fetch('bandform.title').extracted
-        #print "setting attr description: " + data.fetch(
-        #    'bandform.description').extracted
         self.model.attrs['description'] = data.fetch(
             'bandform.description').extracted
+        add_creation_metadata(self.request, self.model.attrs)
 
 
 @tile('editform', interface=Band, permission="edit")
@@ -91,9 +73,6 @@ class BandEditForm(BandForm, Form):
     __plumbing__ = EditPart
 
     def save(self, widget, data):
-        #print "BandEditForm: trying to save form data"
-        #print "title: " + data.fetch('bandform.title').extracted
-        #print "descr: " + data.fetch('bandform.description').extracted
         self.model.attrs['title'] = data.fetch('bandform.title').extracted
         self.model.attrs['description'] = data.fetch(
             'bandform.description').extracted
